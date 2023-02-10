69bea4e3a77e5cd8cc7473f092324593a9540054
Pavion <tvstreamrecord@gmail.com>
2023-02-10 15:02:58 +0100

    Properly encode recording title
    Address #2
    (obviously some white spaces were automatically truncated as well)

--- src/tsrplugin/TsrPlugin.java
+++ src/tsrplugin/TsrPlugin.java
@@ -11,12 +11,14 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -36,11 +38,11 @@ import javax.swing.JOptionPane;
  */
 public class TsrPlugin extends devplugin.Plugin {
     private Properties mSettings;
-    private Config ret; 
+    private Config ret;
 
     private static final util.ui.Localizer mLocalizer
                     = util.ui.Localizer.getLocalizerFor( TsrPlugin.class );
-    
+
     Locale[] supportedLocales = {
         Locale.GERMAN,
         Locale.ENGLISH
@@ -48,16 +50,16 @@ public class TsrPlugin extends devplugin.Plugin {
 
     private static TsrPlugin mInstance;
     public TsrPlugin() {
-        mInstance = this;    
+        mInstance = this;
     }
-    
+
     protected static TsrPlugin getInstance() {
         if (mInstance == null) {
             mInstance = new TsrPlugin();
         }
         return mInstance;
     }
-    
+
     @Override
     public Properties storeSettings() {
         return mSettings;
@@ -68,25 +70,25 @@ public class TsrPlugin extends devplugin.Plugin {
         mSettings = settings;
     }
 
-    public static Version getVersion() {        
+    public static Version getVersion() {
         return new Version(1,3,1,true);
     }
-    
+
     @Override
-    public PluginInfo getInfo() {        
+    public PluginInfo getInfo() {
         String name = mLocalizer.msg( "pluginName" ,"TsrPlugin" );
         String desc = mLocalizer.msg( "pluginDesc" ,"Remote control for tvstreamrecord, a Synology recording package" );
 
-        return new PluginInfo(TsrPlugin.class, name, desc, 
+        return new PluginInfo(TsrPlugin.class, name, desc,
                 "Pavion", "GPL", "http://pavion.github.io/tvstreamrecord/");
     }
-    
+
     @Override
-    public SettingsTab getSettingsTab() {              
-        SettingsTab st = new SettingsTab() {            
-            
+    public SettingsTab getSettingsTab() {
+        SettingsTab st = new SettingsTab() {
+
             @Override
-            public JPanel createSettingsPanel() {                
+            public JPanel createSettingsPanel() {
                 ret = new Config();
                 String urlMessage = mLocalizer.msg( "urlMessage" ,"TVStreamRecord URL and Port (e.g. http://0.0.0.0:8030)" );
                 String url = mSettings.getProperty("URL");
@@ -98,7 +100,7 @@ public class TsrPlugin extends devplugin.Plugin {
             @Override
             public void saveSettings() {
                 if (ret!=null) {
-                    mSettings.setProperty("URL", ret.getURL());                
+                    mSettings.setProperty("URL", ret.getURL());
                 }
             }
 
@@ -112,10 +114,10 @@ public class TsrPlugin extends devplugin.Plugin {
             public String getTitle() {
                 return "TvStreamRecord plugin";
             }
-        }; 
+        };
         return st;
     }
-    
+
     @Override
     public ActionMenu getButtonAction() {
         AbstractAction action = new AbstractAction() {
@@ -135,7 +137,7 @@ public class TsrPlugin extends devplugin.Plugin {
         };
 
         String name = mLocalizer.msg("menuOpen","Open TvStreamRecord");
-        action.putValue(Action.NAME, name);        
+        action.putValue(Action.NAME, name);
 
         // Der Aktion ein kleines Icon geben. Dieses Icon wird im Menü gezeigt
         // Das Icon sollte 16x16 Pixel groß sein
@@ -148,12 +150,12 @@ public class TsrPlugin extends devplugin.Plugin {
         action.putValue(BIG_ICON, icon);
 
         // Das Aktions-Menü erzeugen und zurückgeben
-        return new ActionMenu(action); 
+        return new ActionMenu(action);
     }
 
     @Override
-    public ActionMenu getContextMenuActions(Program program) {        
- 
+    public ActionMenu getContextMenuActions(Program program) {
+
         PluginTreeNode rootNode = getRootNode();
         boolean found = false;
         for (Program p: rootNode.getPrograms()) {
@@ -162,11 +164,11 @@ public class TsrPlugin extends devplugin.Plugin {
                 break;
             }
         }
-        AbstractAction action = null; 
-        
+        AbstractAction action = null;
+
         if (found) {
             action = new AbstractAction() {
-            
+
                 @Override
                 public void actionPerformed(ActionEvent evt) {
                     deleteFromTSR(program);
@@ -174,11 +176,11 @@ public class TsrPlugin extends devplugin.Plugin {
             };
 
             String name = mLocalizer.msg("popupCaptionStop","Cancel capture with TvStreamRecord");
-            action.putValue(Action.NAME, name);        
-            
+            action.putValue(Action.NAME, name);
+
         } else {
             action = new AbstractAction() {
-            
+
                 @Override
                 public void actionPerformed(ActionEvent evt) {
                     sendAnTSR(program);
@@ -186,35 +188,38 @@ public class TsrPlugin extends devplugin.Plugin {
             };
 
             String name = mLocalizer.msg("popupCaptionStart","Capture with TvStreamRecord");
-            action.putValue(Action.NAME, name);        
+            action.putValue(Action.NAME, name);
 
         }
 
         ImageIcon icon = createImageIcon("img/tvstreamrecord.png");
         action.putValue(Action.SMALL_ICON, icon);
 
-        return new ActionMenu(action); 
+        return new ActionMenu(action);
     }
 
     protected void sendAnTSR(Program program) {
         String url = mSettings.getProperty("URL") + "/createtvb";
-            
+
         String recname = program.getTitle();
         String episode = program.getTextField(ProgramFieldType.EPISODE_TYPE);
         if (episode != null) {
             recname += " - " + episode;
         }
-            
-        String sender = program.getChannel().getName();            
+        try {
+            recname = URLEncoder.encode(recname, "UTF-8");
+        } catch (UnsupportedEncodingException ex) {}
+
+        String sender = program.getChannel().getName();
         String von = program.getTimeString();
         String bis = program.getEndTimeString();
         String uniqueid = program.getUniqueID();
-            
+
         Calendar cal = program.getDate().getCalendar();
         String am = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime());
-            
+
         String urlParameters = String.format("recname=%s&sender=%s&von=%s&bis=%s&am=%s&uniqueid=%s", recname, sender, von, bis, am, uniqueid);
-            
+
         String response = connectTsr(url, urlParameters);
 
         if (response.contains("true")) {
@@ -231,39 +236,39 @@ public class TsrPlugin extends devplugin.Plugin {
         String response = connectTsr(url, urlParameters);
 
         if (response.contains("true")) {
-            program.unmark(this);                
+            program.unmark(this);
             getTsrList();
         } else {
             String msg = mLocalizer.msg( "deleteError" ,"Event '{}' could not be stopped" ).replace("{}", program.getTitle());
             JOptionPane.showMessageDialog(this.getParentFrame(), msg, "Error", JOptionPane.ERROR_MESSAGE);
         }
-        
+
     }
-    
+
     protected String connectTsr(String url, String urlParameters) {
-        try {                
+        try {
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            
+
             con.setRequestMethod("POST");
-            
+
             con.setDoOutput(true);
             DataOutputStream wr = new DataOutputStream(con.getOutputStream());
             wr.write(urlParameters.getBytes("UTF-8"));
             wr.flush();
             wr.close();
-            
+
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(con.getInputStream()));
             String inputLine;
             StringBuffer response = new StringBuffer();
-            
+
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
             in.close();
             return response.toString();
-            
+
         } catch (ProtocolException ex) {
             Logger.getLogger(TsrPlugin.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -271,14 +276,14 @@ public class TsrPlugin extends devplugin.Plugin {
             JOptionPane.showMessageDialog(this.getParentFrame(), msg, "Error", JOptionPane.ERROR_MESSAGE);
             Logger.getLogger(TsrPlugin.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return "false";        
+        return "false";
     }
-        
+
     protected void getTsrList() {
         PluginTreeNode rootNode = getRootNode();
         rootNode.removeAllActions();
         rootNode.removeAllChildren();
-  
+
         try {
             String url = mSettings.getProperty("URL") + "/gettvb";
 
@@ -286,13 +291,13 @@ public class TsrPlugin extends devplugin.Plugin {
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
             con.setRequestMethod("GET");
-         
+
             int responseCode = con.getResponseCode();
 
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(con.getInputStream()));
             String inputLine;
-                    
+
             while ((inputLine = in.readLine()) != null) {
                 Program program = getPluginManager().getProgram(inputLine);
                 if (program != null) {
@@ -303,7 +308,7 @@ public class TsrPlugin extends devplugin.Plugin {
             }
             in.close();
             rootNode.update();
-            
+
         } catch (MalformedURLException ex) {
             Logger.getLogger(TsrPlugin.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ProtocolException ex) {
@@ -311,7 +316,7 @@ public class TsrPlugin extends devplugin.Plugin {
         } catch (IOException ex) {
             Logger.getLogger(TsrPlugin.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
     @Override
